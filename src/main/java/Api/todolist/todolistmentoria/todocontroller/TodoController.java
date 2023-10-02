637f775883522836e3920116a79ecef15d22eaa2
Pedro Amaral <peuamaral1@gmail.com>
2023-10-02 17:36:37 -0300

    atualizacao na anotacao

--- src/main/java/Api/todolist/todolistmentoria/todocontroller/TodoController.java
+++ src/main/java/Api/todolist/todolistmentoria/todocontroller/TodoController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,7 @@ import java.util.List;
 @Slf4j
 public class TodoController {
 
+    @Autowired
     TodoService todoService;
 
     @ApiOperation(value = "Criando uma nova tarefa")
--- src/main/java/Api/todolist/todolistmentoria/todorepository/TodoRepopsitory.java
+++ src/main/java/Api/todolist/todolistmentoria/todorepository/TodoRepopsitory.java
@@ -2,7 +2,9 @@ package Api.todolist.todolistmentoria.todorepository;
 
 import Api.todolist.todolistmentoria.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepopsitory extends JpaRepository<Todo, Long> {
 
 
